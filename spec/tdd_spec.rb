45b1fcf3420a569ee6226c3311c00c6032e97f1c
Syeda Juveria Afreen <sja.affu765@gmail.com>
2022-01-31 21:51:33 +0300

    Fixed linter

--- spec/tdd_spec.rb
+++ spec/tdd_spec.rb
@@ -41,4 +41,35 @@ describe Solver do
       expect(factorial_zero).to eq(1)
     end
   end
+
+  context 'Test the fizzbuzz method in Solver Class' do
+    it 'should return fizzbuzz if 15 is passed as number' do
+      string = solve.fizzbuzz(15)
+      expect(string).to eql 'fizzbuzz'
+    end
+    it 'should return fizz if 9 is passed as number' do
+      string = solve.fizzbuzz(9)
+      expect(string).to eql 'fizz'
+    end
+
+    it 'should return buzz if 10 is passed as number' do
+      string = solve.fizzbuzz(10)
+      expect(string).to eql 'buzz'
+    end
+
+    it 'should return buzz if 10 is passed as number' do
+      string = solve.fizzbuzz(20)
+      expect(string).to eql 'buzz'
+    end
+
+    it 'should return 7 as a string it is passed as number' do
+      string = solve.fizzbuzz(7)
+      expect(string).to eql '7'
+    end
+
+    it 'should return 13 as a string it is passed as number' do
+      string = solve.fizzbuzz(13)
+      expect(string).to eql '13'
+    end
+  end
 end
